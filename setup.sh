b4876bc6e32d2894cb7417a63c9af5c5f8e8da1e
alfnpnd <56506924+alfnpnd@users.noreply.github.com>
2021-09-24 10:24:25 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 mkdir -p ~/.streamlit/
-echo "\
-[server]\n\
-port = $PORT\n\
-enableCORS = false\n\
-headless = true\n\
-\n\
-" ~/.streamlit/config.toml
+
+echo "[theme]
+primaryColor = ‘#84a3a7’
+backgroundColor = ‘#EFEDE8’
+secondaryBackgroundColor = ‘#fafafa’
+textColor= ‘#424242’
+font = ‘sans serif’
+[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
