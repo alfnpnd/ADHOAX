40de9fc6ae02c8b24b208ecb1b793edba78fa780
alfnpnd <56506924+alfnpnd@users.noreply.github.com>
2021-09-24 11:37:43 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,7 @@ primaryColor=’#020202’
 backgroundColor=’#c4c3c3’
 secondaryBackgroundColor=’#ebd316’
 font = ‘sans serif’
-\
-[server]\n\
+\[server]\n\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
