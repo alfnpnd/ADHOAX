1c9bb26f47383ba5a7c8c8f82cc5056582bbf28d
alfnpnd <56506924+alfnpnd@users.noreply.github.com>
2021-09-26 21:55:08 +0700

    Update adhoax.py

--- adhoax.py
+++ adhoax.py
@@ -14,8 +14,8 @@ hide_menu = """
 """
 st.markdown(hide_menu, unsafe_allow_html=True)
 
-load_clf = pickle.load(open('DTModel79.pkl', 'rb'))
-load_vectorizer = pickle.load(open("vectorizerNew.pkl", "rb"))
+load_clf = pickle.load(open('DTModel78.pkl', 'rb'))
+load_vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
 
 
 st.title('ADHOAX')
